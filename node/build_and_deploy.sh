d0cc1489183a0c54513c00c8f69d4491825972a3
Yury Baykov <yury.baykov@mail.ru>
2018-12-01 11:33:01 +0300

    printing svc

--- node/build_and_deploy.sh
+++ node/build_and_deploy.sh
@@ -6,3 +6,4 @@ sudo docker push icp.bmstu.ru:8500/team02/team02_day4_node
 kubectl run team02node --image=icp.bmstu.ru:8500/team02/team02_day4_node
 kubectl expose deployment team02node --name=team02-node-http --type=LoadBalancer --port=80 --target-port=80
 kubectl expose deployment team02node --name=team02-node-ssh --type=LoadBalancer --port=22 --target-port=22
+kubectl get svc
