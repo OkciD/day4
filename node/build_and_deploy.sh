9505db24d7daa3310de97610df3e9206da79ef0f
Yury Baykov <yury.baykov@mail.ru>
2018-12-01 11:28:47 +0300

    i'm tired of typing it, seriously

---
+++ node/build_and_deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo docker build -t team02_day4_node ./
+sudo docker tag team02_day4_node icp.bmstu.ru:8500/team02/team02_day4_node
+sudo docker push icp.bmstu.ru:8500/team02/team02_day4_node
+kubectl run team02node --image=icp.bmstu.ru:8500/team02/team02_day4_node
+kubectl expose deployment team02node --name=team02-node-http --type=LoadBalancer --port=80 --target-port=80
+kubectl expose deployment team02node --name=team02-node-ssh --type=LoadBalancer --port=22 --target-port=22
